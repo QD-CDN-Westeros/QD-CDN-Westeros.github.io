3fa2a63c4f137acffa1c8002e696c9ac3d414488
Giovana <giovana@quatrodigital.com.br>
2018-09-13 16:26:25 -0300

    🐱‍🐉

--- auteco-arquivos/auteco-motos.min.js
+++ auteco-arquivos/auteco-motos.min.js
@@ -1,2 +1,2 @@
-/* Wartodle - 13/09/2018 16:22:05 GMT-0300 */
+/* Wartodle - 13/09/2018 16:25:42 GMT-0300 */
 var qdStoreLocator_jsMotos={moto:[{title:"NS 160 PRO",brand:"Pulsar",displacement:"0km",max_power:"15.3 HP @ 8500 rpm",max_torque:"14.6 Nm @ 6500 rpm",transmission:"14:42"},{title:"200 DUKE",brand:"KTM",displacement:"10000km",max_power:"25.5 HP @ 10000 rpm",max_torque:"14.6 Nm @ 6500 rpm",transmission:"22:72"}]};
