1f99e36ad8bdc757f5e01b85f2d076942eb61b83
Giovana <giovana@quatrodigital.com.br>
2018-10-18 18:01:34 -0300

    🐱‍🐉

--- auteco-arquivos/auteco-motos.js
+++ auteco-arquivos/auteco-motos.js
@@ -1,2 +1,2 @@
-/* Wartodle - 18/10/2018 13:46:09 GMT-0300 */
+/* Wartodle - 18/10/2018 18:00:33 GMT-0300 */
 var QD_jsMotos={mydata:[{key:"Pulsar",value1:"NS 160 PRO",value2:"",value3:"",value4:"",value5:"",value6:""},{key:"KTM",value1:"200 DUKE",value2:"",value3:"",value4:"",value5:"",value6:""}]};
