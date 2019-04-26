cb6e243662d5061fc617b7a5b40bd0684de40c28
Gustavo Borges <gustavo.borges.devfc@gmail.com>
2019-04-26 15:18:14 -0300

    ...

---
+++ cars-arquivos/JS.c.car-main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".header-form-blockcar form fieldset select").remove()
+    $(".header-form-blockcar form fieldset legend").remove()
+    $(".header-form-blockcar form fieldset input[type='hidden']").remove()
+    $(" .header-form-blockcar form fieldset input[type='button']").remove()
+    $(".header-form-blockcar form fieldset label").remove()
+})
