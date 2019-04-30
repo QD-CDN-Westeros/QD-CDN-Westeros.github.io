190a581474239d6d25a2641dfe66a50c8dcdd4c9
Gustavo Borges <gustavo.borges.devfc@gmail.com>
2019-04-30 13:57:13 -0300

    ---

--- cars-arquivos/JS.c.car-main.js
+++ cars-arquivos/JS.c.car-main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".header-form-blockcar form fieldset select").remove()
     $(".header-form-blockcar form fieldset legend").remove()
     $(".header-form-blockcar form fieldset input[type='hidden']").remove()
-    $(" .header-form-blockcar form fieldset input[type='button']").remove()
+    $(".header-form-blockcar form fieldset input[type='button']").remove()
     $(".header-form-blockcar form fieldset label").remove()
+    $("#ajaxBusy").remove()
 })
