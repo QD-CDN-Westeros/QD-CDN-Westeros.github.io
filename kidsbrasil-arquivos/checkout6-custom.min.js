f401fa64a589fde497129caded2607eb1f399834
David <david@maeztra.com>
2020-03-17 18:00:55 -0300

    ---

--- kidsbrasil-arquivos/checkout6-custom.min.js
+++ kidsbrasil-arquivos/checkout6-custom.min.js
@@ -1,2 +1,2 @@
-/* PIKACHU - 12/03/2020 16:53:17 GMT */
+/* DESKTOP-NA07EK9 - 16/03/2020 09:43:38 GMT */
 function checkoutSteps(){$(window).on("hashchange",function(e){if(window.location.hash=="#/cart"){$(".mz-header__checkout--steps li").removeClass("active");$("#sacola").addClass("active")}else if(window.location.hash=="#/email"||window.location.hash=="#/profile"){$(".mz-header__checkout--steps li").removeClass("active");$("#identificacao").addClass("active")}else if(window.location.hash=="#/payment"){$(".mz-header__checkout--steps li").removeClass("active");$("#pagamento").addClass("active")}else if(window.location.hash=="/orderPlaced"){$(".mz-header__checkout--steps li").removeClass("active");$("#confirmacao").addClass("active")}})}checkoutSteps();
