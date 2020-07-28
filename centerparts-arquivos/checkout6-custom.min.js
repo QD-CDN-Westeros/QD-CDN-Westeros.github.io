de6d282a76720869da06b8eac170f67a21d04423
Developers Quatro Digital <dev@quatrodigital.com.br>
2020-07-28 11:24:19 -0300

    center parts wishlist / search

--- centerparts-arquivos/checkout6-custom.min.js
+++ centerparts-arquivos/checkout6-custom.min.js
@@ -1,2 +1,2 @@
-/* MEWTWO - 17/06/2020 10:11:54 GMT */
+/* MEWTWO - 27/07/2020 18:11:33 GMT */
 function checkoutSteps(){$(window).on("hashchange",function(e){if(window.location.hash=="#/cart"){$(".mz-header__checkout--steps li").removeClass("active");$("#sacola").addClass("active")}else if(window.location.hash=="#/email"||window.location.hash=="#/profile"){$(".mz-header__checkout--steps li").removeClass("active");$("#identificacao").addClass("active")}else if(window.location.hash=="#/payment"){$(".mz-header__checkout--steps li").removeClass("active");$("#pagamento").addClass("active")}else if(window.location.hash=="/orderPlaced"){$(".mz-header__checkout--steps li").removeClass("active");$("#confirmacao").addClass("active")}})}checkoutSteps();
