9d08ae96cd204f1c2b50acd7614682c8477841d9
David Lucas Blanco <david.blanco@maeztra.com>
2020-08-14 10:45:37 -0300

    Centerparts Removendo itens da loja anterior e fixed bar

--- centerparts-arquivos/checkout6-custom.js
+++ centerparts-arquivos/checkout6-custom.js
@@ -1,2 +1,2 @@
-/* MEWTWO - 27/07/2020 18:11:33 GMT */
+/* davids - 14/08/2020 08:11:06 GMT-0300 */
 function checkoutSteps(){$(window).on("hashchange",function(e){if(window.location.hash=="#/cart"){$(".mz-header__checkout--steps li").removeClass("active");$("#sacola").addClass("active")}else if(window.location.hash=="#/email"||window.location.hash=="#/profile"){$(".mz-header__checkout--steps li").removeClass("active");$("#identificacao").addClass("active")}else if(window.location.hash=="#/payment"){$(".mz-header__checkout--steps li").removeClass("active");$("#pagamento").addClass("active")}else if(window.location.hash=="/orderPlaced"){$(".mz-header__checkout--steps li").removeClass("active");$("#confirmacao").addClass("active")}})}checkoutSteps();
