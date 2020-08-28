0d7d642773068b402594a4ffdb5ea74683d039b8
Rogerio Vieira <rogerio@maeztra.com>
2020-08-28 11:37:19 -0300

    subindo movize

---
+++ movize/js/b8one.mobile.account.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{13:function(n,o,p){n.exports=p("W67v")},W67v:function(n,o){}},[[13,0,2]]]);
+//# sourceMappingURL=b8one.mobile.account.js.map
