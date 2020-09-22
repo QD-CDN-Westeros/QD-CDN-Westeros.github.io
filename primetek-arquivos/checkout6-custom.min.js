9301d8eadb286c297160907fed56483ae8de99e0
Danilo <web.medeiros@gmail.com>
2020-09-22 08:20:21 -0300

    ---

--- primetek-arquivos/checkout6-custom.min.js
+++ primetek-arquivos/checkout6-custom.min.js
@@ -1,2 +1,2 @@
-/* DESKTOP-US8NRQT - 14/09/2020 14:12:01 GMT */
+/* DESKTOP-US8NRQT - 15/09/2020 10:46:31 GMT */
 function fixInput(){$(window).load(function(){$("#ship-receiverName").on("keydown",function(e){if(e.keyCode==8&&$(this).val().length==1){$(this).val("")}})})}fixInput();
