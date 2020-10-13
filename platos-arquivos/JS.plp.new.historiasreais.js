2d0a852140f870e03c0d0b9cad0de850217e3a08
Gabriel Leocadio <gabrielsilva94@gmail.com>
2020-10-13 15:51:00 -0300

    ---

---
+++ platos-arquivos/JS.plp.new.historiasreais.js
@@ -0,0 +1,2 @@
+/* PC-QUARTO - 13/10/2020 15:50:29 GMT */
+"use strict";(function(){try{var HistoriasReais={init:function init(){console.log("[HISTORIAS REAIS]")}};HistoriasReais.init()}catch(e){console.log(e)}})();
