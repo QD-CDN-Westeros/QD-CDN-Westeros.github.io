9b25bd7a087f088b6b9e884e34ba18aab91fa2a7
Cristian Andrade <cristian@maeztra.com>
2021-04-06 12:41:50 -0300

    ---------

--- wnf-arquivos/checkout6-custom.js
+++ wnf-arquivos/checkout6-custom.js
@@ -1,2 +1,2 @@
-/* DESKTOP-2VAUUHI - 23/02/2021 12:16:42 GMT */
+/* Lugia - 06/04/2021 12:37:51 GMT */
 "use strict";var Checkout={init:function init(){Checkout.warningCheckout()},warningCheckout:function warningCheckout(){setTimeout(function(){$('<div class="warning_checkout" style="display:none"><p>Antes de finalizar o pedido, confirme se o seu endereço de entrega está completo e correto</p></div>').insertBefore(".container.container-main")},3500);$(window).on("hashchange",function(){var hash=window.location.hash;if(hash=="#/profile"||hash=="#/shipping"){$(".warning_checkout").addClass("active")}if(hash=="#/payment"){$(".warning_checkout").addClass("active")}})}};$(function(){Checkout.init()});
