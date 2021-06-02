90f1dec615507e122e91434f1e7a27bb4fdd71bf
Thiagofrf <thiago.fontanella@hotmail.com>
2021-06-02 17:14:44 -0300

    [Flor de Lis] Feat: criando padrão de arquivos JS do novo repositório

--- flordelis-arquivos/checkout6-custom.js
+++ flordelis-arquivos/checkout6-custom.js
@@ -1,2 +1,2 @@
-/* eduardo - 01/06/2021 18:40:40 GMT */
+/* LAPTOP-T0P1H0OI - 02/06/2021 17:01:53 GMT */
 "use strict";var checkoutCustom={init:function init(){checkoutCustom.changeLabelName()},stopAjax:function stopAjax(){checkoutCustom.changeLabelName()},changeLabelName:function changeLabelName(){$(".box-client-info-pf .client-first-name label").html("Nome");$(".box-client-info-pf .client-last-name label").html("Sobrenome")}};$(function(){checkoutCustom.init()});$(document).ajaxStop(function(){checkoutCustom.stopAjax()});
