bd1c0a1f5a45fc55173651acecec6efbe25e16ec
ViniverissimoMaeztra <105666212+ViniverissimoMaeztra@users.noreply.github.com>
2023-05-10 12:57:37 -0300

    [Sissla]

---
+++ sislla-arquivos/JS.sislla.functions.product.js
@@ -0,0 +1,2 @@
+/* DESKTOP-F6V71R2 - 10/05/2023 08:56:21 GMT */
+var Product={init:function(){Product.teste()},ajaxStop:function(){},windowOnload:function(){},teste:function(){console.log("teste")}};
