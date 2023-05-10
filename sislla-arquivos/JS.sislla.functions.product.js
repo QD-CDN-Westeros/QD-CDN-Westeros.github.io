f16b0b3a3b763a22661ea4917c1a0ae7ee95e367
ViniverissimoMaeztra <105666212+ViniverissimoMaeztra@users.noreply.github.com>
2023-05-10 20:59:28 -0300

    [Sislla] feat: initial commit

--- sislla-arquivos/JS.sislla.functions.product.js
+++
@@ -1,2 +0,0 @@
-/* DESKTOP-F6V71R2 - 10/05/2023 08:56:21 GMT */
-var Product={init:function(){Product.teste()},ajaxStop:function(){},windowOnload:function(){},teste:function(){console.log("teste")}};
